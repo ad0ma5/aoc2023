85cb6e050a2969ff4ed3f5ec8f92fd82fae75e36
ad0ma5 <donatas.adamonis@gmail.com>
2024-08-30 13:12:31 +0100

    save 12

--- day12.go
+++ day12.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	//"log"
 	"os"
 	"fmt"
 	"strings"
@@ -11,10 +11,11 @@ import (
 
 
 func main() {
-	data, err := os.ReadFile("input12s.txt")
+    file:="input12s.txt"
+	data, err := os.ReadFile(file)
 	if err != nil {
 		//log.Fatal(err)
-	fmt.Println("not ok file")
+	fmt.Println("not ok file", file)
 
 	}
 	str:=string(data);
@@ -26,16 +27,32 @@ func main() {
 	  larry:=strings.Split(larr[i], " ")
 	  templ:=strings.Split(larry[1], ",")
 	  expected:=""
+      riddle:=larry[0]
 	  for t:=0;t<len(templ);t++{
 	    tint,_:=strconv.Atoi(templ[t])
 	    for ti:=0;ti<tint;ti++ {
 	      expected+="#"
 	    }
-	    expected+=" "
-	  }
-	  fmt.Println(larr[i], expected)
-
+        if t < len(templ)-1 {
+	      expected+="."
         }
+	  }
+
+	  fmt.Println(riddle, "expected", expected, templ)
+      if len(expected) == len(riddle) {
+          fmt.Println("EQUAL: ", expected)
+          continue
+      }
+      ec:=0
+      for c:=0;c<len(riddle); c++ {
+          if larr[i][c] != expected[ec]{
+              fmt.Println(c, " not equal" ,larry[i][c], expected[c])
+          }else{
+              fmt.Println(c, " equal" ,larry[i][c], expected[c])
+          }
+      }
+
+    }
 	fmt.Println("ok")
         //os.Stdout.Write(l);
 	//os.Stdout.Write(data)
